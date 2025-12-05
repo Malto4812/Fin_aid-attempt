a8e84eef8b6d667c70eca120cca8c8753ea2e65c
Malto4812 <mattmukisa44@gmail.com>
2025-12-05 11:05:10 +0300

    Update app_pytz.py

--- app_pytz.py
+++ app_pytz.py
@@ -373,5 +373,6 @@ def admin_password_file():
 
 if __name__ == "__main__":
     # For Render, listen on all interfaces and pick port from environment
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
